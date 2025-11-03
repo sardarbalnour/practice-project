4c06c1dca6ff0266025c7c171e84cf7ba21fed86
Sardar Balnour <balnour.sardar@gmail.com>
2025-11-03 12:09:00 +0330

    E1-42

--- main.js/main.js
+++ main.js/main.js
@@ -1,2 +1,17 @@
 console.log("hello again i'm back =)))");
 
+//unary operators
+const value = "-5";
+console.log(+value);
+
+//nullish coalescing operator
+let name;
+const num = 2;
+console.log(name ?? num);
+
+const str = "slaw mama jian vaazt chona?";
+console.log(str.split(" "));
+console.log(str.length);
+
+//template literals
+console.log(`${str} qurbant bm kaka jian!`);
