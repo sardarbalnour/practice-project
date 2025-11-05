b82143b89a71d76b2c4827412f873fc96ea5e4de
Sardar Balnour <balnour.sardar@gmail.com>
2025-11-05 12:01:44 +0330

    E56-74

--- main.js/main.js
+++ main.js/main.js
@@ -5,8 +5,8 @@ const value = "-5";
 // console.log(+value);
 
 //nullish coalescing operator
-let name;
-const num = 2;
+// let name;
+// const num = 2;
 // console.log(name ?? num);
 
 const str = "slaw mama jian vaazt chona?";
@@ -16,23 +16,32 @@ const str = "slaw mama jian vaazt chona?";
 //template literals
 // console.log(`${str} qurbant bm kaka jian!`);
 
-const array = ["ali", "mamad", "asqar", "ahmad"];
-array.push("sardar");
-array.splice(0, 1, "sard");
-console.log(array);
+// const array = ["ali", "mamad", "asqar", "ahmad"];
+// array.push("sardar");
+// array.splice(0, 1, "sard");
+// console.log(array);
 // console.log(array);
 
-const array2 = [];
-array2.push("fdbal");
-array2.push("baskitbal");
-array2.push("valibal");
-array2.push("moz");
-const stringSports = array2.join();
-console.log(`my favorite sports are ${stringSports}`);
-const lastIndex = array2.length - 1;
-array2.splice(lastIndex);
-const firstTwoSports = array2.slice(0, 2);
-console.log(firstTwoSports);
-console.log(array2);
-array2.splice(0, 2, firstTwoSports);
-console.log(array2);
+// const array2 = [];
+// array2.push("fdbal");
+// array2.push("baskitbal");
+// array2.push("valibal");
+// array2.push("moz");
+// const stringSports = array2.join();
+// console.log(`my favorite sports are ${stringSports}`);
+// const lastIndex = array2.length - 1;
+// array2.splice(lastIndex);
+// const firstTwoSports = array2.slice(0, 2);
+// console.log(firstTwoSports);
+// console.log(array2);
+// array2.splice(0, 2, firstTwoSports);
+// console.log(array2);
+
+const age = 24;
+const data = {
+  name: "sardar",
+  age: 25,
+};
+console.log(data);
+const { age: age2, name } = data;
+console.log(name,age2);
