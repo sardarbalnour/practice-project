7c848174b69b04187f14a0d07709bfb8143189c1
Sardar Balnour <balnour.sardar@gmail.com>
2025-11-11 10:54:10 +0330

    E134-144

--- main.js/main.js
+++ main.js/main.js
@@ -1,4 +1,14 @@
-const date = new Date()
-console.log(date.getTime());
-console.log(date.toLocaleTimeString());
+// const date = new Date();
+// console.log(date.getTime());
+// console.log(date.toLocaleTimeString());
 
+const myMap = new Map();
+myMap.set("name", "sardar").set("age", 32).set(5, 20);
+console.log(myMap);
+
+const age = Symbol("age")
+const data = {
+    name1:"sardar",
+    lastName:"balnour",
+    [age]:22,
+}
