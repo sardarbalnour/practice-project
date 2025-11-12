660a32f2101b4f0d2e03669b2d53ec781d9dc95c
Sardar Balnour <balnour.sardar@gmail.com>
2025-11-12 23:07:31 +0330

    bad day but still alive E145-147

--- main.js/main.js
+++ main.js/main.js
@@ -1,14 +1,10 @@
-// const date = new Date();
-// console.log(date.getTime());
-// console.log(date.toLocaleTimeString());
-
-const myMap = new Map();
-myMap.set("name", "sardar").set("age", 32).set(5, 20);
-console.log(myMap);
-
-const age = Symbol("age")
-const data = {
-    name1:"sardar",
-    lastName:"balnour",
-    [age]:22,
+const playerChoice = prompt("shteki leda day");
+console.log(playerChoice);
+const choices = ["rock", "paper", "scissors"];
+const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+console.log(computerChoice);
+if (playerChoice === computerChoice) {
+  return "it's a tie";
+} else if (playerChoice === "rock") {
+  return computerChoice === "scissors" ? "player" : "computer";
 }
