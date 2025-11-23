6648600bb1206801bdef547ee578629d2bde094d
Sardar Balnour <balnour.sardar@gmail.com>
2025-11-23 11:30:50 +0330

    first day after sarbazi E151

--- main.js/main.js
+++ main.js/main.js
@@ -1,10 +1,47 @@
-const playerChoice = prompt("shteki leda day");
-console.log(playerChoice);
 const choices = ["rock", "paper", "scissors"];
-const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-console.log(computerChoice);
-if (playerChoice === computerChoice) {
-  return "it's a tie";
-} else if (playerChoice === "rock") {
-  return computerChoice === "scissors" ? "player" : "computer";
-}
+let playerScore = 0;
+let computerScore = 0;
+
+const checkWinner = (player, computer) => {
+  if (player === computer) {
+    return "draw";
+  } else if (player === "rock") {
+    return computer === "scissors" ? "player" : "computer";
+  } else if (player === "paper") {
+    return computer === "scissors" ? "computer" : "player";
+  } else {
+    return computer === "rock" ? "computer" : "player";
+  }
+};
+
+const showResult = (result) => {
+  if (result === "player") {
+    console.log("you won");
+    playerScore++;
+  } else if (result === "computer") {
+    console.log("computer won");
+    computerScore++;
+  } else {
+    console.log("it's a tie");
+  }
+  console.log(`Your score is: ${playerScore}`);
+  console.log(`Computer score is: ${computerScore}`);
+  console.log("------------------------------------");
+};
+
+const play = () => {
+  const playerChoice = prompt("shteki leda day");
+  if (choices.indexOf(playerChoice?.toLowerCase()) !== -1) {
+    console.log(`You choose ${playerChoice.toLowerCase()}`);
+  } else {
+    console.log("You cheated");
+    return;
+  }
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  console.log(`computer chooses ${computerChoice}`);
+  const gameResult = checkWinner(playerChoice, computerChoice);
+  showResult(gameResult);
+  play()
+};
+
+play();
