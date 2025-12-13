9ee50031cf743a76f55018dc2007ecdffd88c5d2
Sardar Balnour <balnour.sardar@gmail.com>
2025-12-13 11:16:23 +0330

    show alert fuction

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
+const alertMessage = document.getElementById("alert-message");
 
 const todos = [];
+
+const showAlert = (message, type) => {
+  alertMessage.innerHTML = "";
+  const alert = document.createElement("P");
+  alert.innerText = message;
+  alert.classList.add("alert");
+  alert.classList.add(`alert-${type}`);
+  alertMessage.append(alert);
+
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+};
 
 const addHandler = () => {
   const task = taskInput.value;
@@ -16,8 +30,9 @@ const addHandler = () => {
     todos.push(todo);
     taskInput.value = "";
     dateInput.value = "";
+    showAlert("Todo added successfully.", "success");
   } else {
-    alert("warning");
+    showAlert("Please add a todo!", "error");
   }
 };
 
