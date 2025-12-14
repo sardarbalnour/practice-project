ca1bc4b8f0911bbfadc62aa5f4a57278990d847e
Sardar Balnour <balnour.sardar@gmail.com>
2025-12-14 12:14:59 +0330

    deleteAllHandler

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
 const alertMessage = document.getElementById("alert-message");
 const todosBody = document.querySelector("tbody");
+const deleteAllButton = document.getElementById("delete-all-button");
 
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 const generateId = () => {
   return Math.round(
@@ -73,5 +74,17 @@ const addHandler = () => {
   }
 };
 
-window.addEventListener("load",displayTodos)
+const deleteAllHandler = () => {
+  if (todos.length) {
+    todos = [];
+    saveToLocalStorage();
+    displayTodos();
+    showAlert("All todos cleared successfully.", "success");
+  } else {
+    showAlert("No todos to clear!", "error");
+  }
+};
+
+window.addEventListener("load", displayTodos);
 addButton.addEventListener("click", addHandler);
+deleteAllButton.addEventListener("click", deleteAllHandler);
